96d32b982104c6dd52e244a027f52a13e1292500
jybak <daum4312@gmail.com>
2019-08-03 16:07:36 +0900

    I hate stars

---
+++ 1-3 Report.py
@@ -0,0 +1,39 @@
+# i= [70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+# sum = 0
+#
+# for a in i:
+#     print(a)
+#     sum = sum+a
+#
+# avg = sum/10
+# print(avg)
+sum = 0
+for i in range(1, 101):
+    print(i)
+    sum = sum + i
+print(sum)
+avg = sum / 100
+print(avg)
+sum = sum+i[0]
+sum = sum+i[1]
+sum = sum+i[2]
+sum = sum+i[3]
+sum = sum+i[4]
+sum = sum+i[5]
+sum = sum+i[6]
+sum = sum+i[7]
+sum = sum+i[8]
+sum = sum+i[9]
+#
+# avg = sum/10
+# print(avg)
+#
+
+
+#
+#
+# total= 0
+# for score in i:
+#     total+i=
+# average= sum/
+# print(average)
